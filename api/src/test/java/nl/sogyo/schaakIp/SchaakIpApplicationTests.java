13b9d780dec6d8b7b64c951a4e5b069f78c05dbb
Frank Fluit <ffluit@sogyo.nl>
2023-10-09 13:45:12 +0200

    initialized client and api

---
+++ api/src/test/java/nl/sogyo/schaakIp/SchaakIpApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.sogyo.schaakIp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchaakIpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
