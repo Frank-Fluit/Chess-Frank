258b981ab81570c0b53a3a74355c0b0d32518577
Frank Fluit <ffluit@sogyo.nl>
2023-10-11 17:54:25 +0200

    changed client somewhat and started implementing database

--- clientts/src/App.tsx
+++ clientts/src/App.tsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import chessboardImage from './assets/chessImage.jpg';
+import chessboard from './assets/chessboard.jpg';
 
 function App() {
   const [count, setCount] = useState(0);
   const [greeting, setGreeting] = useState('');
+  const [player1Name, setPlayer1Name] = useState('');
+  const [player2Name, setPlayer2Name] = useState('');
 
   const fetchGreeting = () => {
 
@@ -31,26 +35,50 @@ function App() {
       });
   };
 
+
+
   return (
     <>
-
       <h1><b>ChessGame Individual Project Sogyo Frank Fluit</b></h1>
       <div className="card">
-        <button onClick={fetchGreeting}>Click <b>here</b> to start game</button>
-
+        <div className="space-x-4">
+          <p className="text-center ">Please enter your names:</p> {/* Prompt text */}
+          <input
+            type="text"
+            placeholder="Player 1 Name"
+            value={player1Name}
+            onChange={(e) => setPlayer1Name(e.target.value)}
+            className="text-center p-2 border border-black rounded-lg" // Add borders here
+          />
+          <input
+            type="text"
+            placeholder="Player 2 Name"
+            value={player2Name}
+            onChange={(e) => setPlayer2Name(e.target.value)}
+            className="text-center p-2 border border-black rounded-lg" // Add borders here
+          />
+        </div>
+        <button onClick={fetchGreeting}>Click <b>here</b> to start the game</button>
         {greeting && <p>GET request result: {greeting}</p>}
       </div>
 
       <div className="card">
-        <button onClick={fetchGreeting}>Click <b>here</b> to do a move </button>
-        <br />
-        [input move to be implemented]
-
+        <img src={chessboard} alt="Chessboard" className="mx-auto p-0.1 w-1/3" />
       </div>
 
+
+
+      <div className="card">
+        <button onClick={fetchGreeting}>Click <b>here</b> to make a move</button>
+        <br />
+        [input move to be implemented]
+      </div>
+
+      <div className="card">
+        <img src={chessboardImage} alt="Chessboard" className="mx-auto p-4 w-1/2" />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
