21565829af9d769648efd8d9a81151f78210a88d
Frank Fluit <ffluit@sogyo.nl>
2023-10-12 17:48:35 +0200

    added a start for a domove implementation

--- domain/src/main/java/chess/domain/Piece.java
+++ domain/src/main/java/chess/domain/Piece.java
@@ -1,4 +1,29 @@
 package chess.domain;
 
 public abstract class Piece {
+    int row;
+    int col;
+    Board board;
+
+    public Piece() {
+
+    }
+
+    public Piece(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+
+    void doMove(int targetRow, int targetCol){
+        //TODO : legalmovecheck
+        this.board.getSquares(this.row,this.col).empty();
+        Square targetSquare = this.board.getSquares(targetRow,targetCol);
+        targetSquare.update(this);
+        this.row = targetRow;
+        this.col =targetCol;
+
+    }
 }
+
+
+
