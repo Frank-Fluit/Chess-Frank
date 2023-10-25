84e2e9b59af946e5894019fd085ae10873bb73b5
Frank Fluit <ffluit@sogyo.nl>
2023-10-25 12:12:44 +0200

    added logging and querying database

--- api/src/main/java/nl/sogyo/chess/chesscontroller/GameHistoryEndPoint.java
+++ api/src/main/java/nl/sogyo/chess/chesscontroller/GameHistoryEndPoint.java
@@ -4,25 +4,30 @@ import chess.domain.Playable;
 import jakarta.servlet.http.HttpSession;
 import nl.sogyo.chess.chesscontroller.models.BoardDTO;
 import nl.sogyo.chess.chesscontroller.models.MoveDTO;
+import nl.sogyo.chess.repository.WinnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/gamehistory")
 public class GameHistoryEndPoint {
 
+    private final WinnerRepository winnerRepository;
+
+    @Autowired
+    public GameHistoryEndPoint(WinnerRepository winnerRepository) {
+        this.winnerRepository = winnerRepository;
+    }
+
 
     @GetMapping(produces = "application/json")
 
-    public String getGameHistory() {
-
-
-        //TODO
-        //Get winner from database here
-
-
-        return "GameHistory";
+    public List<Object[]> getGameHistory() {
+        List<Object[]> winners = winnerRepository.countWinners();
+        return winners;
 
     }
 
--- clientMancalaBase/src/pages/MatchHistory.tsx
+++ clientMancalaBase/src/pages/MatchHistory.tsx
@@ -1,51 +1,54 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react';
 
 const MatchHistory = () => {
+  const [matchHistory, setMatchHistory] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-const [matchHistory, setMatchHistory] = useState([]);
-const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    // Use the useEffect hook to fetch match history data when the component mounts
+    getMatchHistory();
+  }, []);
 
-async function getMatchHistory() {
-      try {
-        const response = await fetch("api/gamehistory", {
-          method: "GET",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
+  async function getMatchHistory() {
+    try {
+      const response = await fetch("api/gamehistory", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
 
-        });
-
-        if (response.ok) {
-          const matchHistory = await response.json();
-          setMatchHistory(matchHistory);
-        } else {
-          console.error(`HTTP error! Status: ${response.status}`);
-        }
-      } catch (error) {
-        console.error('Fetch error:', error);
+      if (response.ok) {
+        const matchHistoryData = await response.json();
+        setMatchHistory(matchHistoryData);
+        setLoading(false); // Update the loading state
+      } else {
+        console.error(`HTTP error! Status: ${response.status}`);
       }
+    } catch (error) {
+      console.error('Fetch error:', error);
     }
+  }
 
   return (
     <div>
-      <h2>Match History</h2>
+      <h2>Leaderboard</h2>
 
-
-
-
-      {/* //TODO TODO TODO*/}
-
-      {/* Add a fetch call to the MatchHistory here*/}
-      {/* Parse the response of fetch call to show historic results*/}
-
-
-
-
-
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {matchHistory.map((match, index) => (
+            <li key={index}>
+              {/* Display the match history data here */}
+              {match[0] + " " + match[1] }
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
 
 export default MatchHistory;
